b35cd4f6356ad11b343827f31779cd125416f350
kyp2975 <kyp2975@synapsoft.co.kr>
2019-02-20 10:58:50 +0900

    add java compile, TODO FIXME comment

--- mods/file_runner.py
+++ mods/file_runner.py
@@ -1,23 +1,42 @@
+import os
 import subprocess
 from subprocess import PIPE
 from mods import CodeType
 
 
+def _replace_multiple(main_string, to_be_replaces, new_string):
+    for elem in to_be_replaces:
+        if elem in main_string:
+            main_string = main_string.replace(elem, new_string)
+
+    return main_string
+
+
+def _permission_change(file_path):
+    os.chmod(file_path, 0o755)
+
+
 def _java_compile(file_path):
-    subprocess.Popen(['javac', file_path], shell=True)
+    subprocess.Popen(['javac', file_path])
+    return os.path.splitext(file_path)[0]
 
 
 def run_file(code_type, file_path):
     if code_type == CodeType.PYTHON:
         sub_p = subprocess.Popen(['python', file_path], stdout=PIPE)
     elif code_type == CodeType.SHELL:
-        sub_p = subprocess.Popen([file_path], stdout=PIPE)
+        _permission_change(file_path)  # FIXME control file owner
+        sub_p = subprocess.Popen(file_path, stdout=PIPE)
     elif code_type == CodeType.JAVA:
         _java_compile(file_path)
-        sub_p = subprocess.Popen(['java', file_path], stdout=PIPE)  # FIXME control file owner
+        dir_path = os.path.dirname(file_path)
+        file_path = _java_compile(file_path)
+        _permission_change(file_path)  # FIXME control file owner
+        execute_path = _replace_multiple(file_path, '/', '.')
+        sub_p = subprocess.Popen(['java', '-cp', dir_path, execute_path], stdout=PIPE, shell=True)
     elif code_type == CodeType.JAVASCRIPT:
         _java_compile(file_path)
-        sub_p = subprocess.Popen(['node', file_path], stdout=PIPE)  # FIXME control file owner
+        sub_p = subprocess.Popen(['node', file_path], stdout=PIPE)
     else:
         return 'error'  # TODO error exception raise
 
--- mods/file_writer.py
+++ mods/file_writer.py
@@ -4,12 +4,13 @@ import time
 
 
 def _get_data_directory():
-    cur_path = os.path.dirname(__file__)
-    data_dir = 'data/'
+    cur_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    data_dir = 'data'
     return os.path.join(cur_path, data_dir)
 
 
 def _gen_file_name(code_type):
+    # FIXME java filename
     now_time = time.localtime()
     random_name = str(uuid.uuid4()).split('-')[4]
     temp_file_name = '{}-{}-{}_{}.{}'.format(
